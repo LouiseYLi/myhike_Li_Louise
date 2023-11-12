abd9d7ff5f570b5fed95944ea892089809aaa8d3
Louise Li <louiseli.van@gmail.com>
2023-11-11 17:39:51 -0800

    further changes applied

--- scripts/profile.js
+++ scripts/profile.js
@@ -37,5 +37,23 @@ function editUserInfo() {
     document.getElementById('personalInfoFields').disabled = false;
     }
 
+function saveUserInfo() {
+    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
+    userSchool = document.getElementById('schoolInput').value;     //get the value of the field with id="schoolInput"
+    userCity = document.getElementById('cityInput').value;       //get the value of the field with id="cityInput"
+
+    currentUser.update ({
+        name: userName,
+        school: userSchool,
+        city: userCity
+    })
+    .then(() => {
+        console.log("Document updated successfully.");
+    })
+
+    document.getElementById('personalInfoFields').disabled = true;
+
+}
+
 //call the function to run it 
 populateUserInfo();
